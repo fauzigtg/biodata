d09238ea712fcc38ae7257afe8a70f1b647b965d
fauzigtg <72981720+fauzigtg@users.noreply.github.com>
2020-11-05 17:03:14 +0700

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mohammad__Fauzi
+ */
+public class biodata {
+    public void infoNama() {
+        System.out.println("Nama : MOHAMMAD FAUZI"); 
+    }
+    
+    public void infoKelas() {
+        System.out.println("Kelas : XI RPL 1");
+    }
+    
+    public void infoAbsen() {
+        System.out.println("No.absen : 27");
+    }
+    
+    public void infoTTL() {
+        System.out.println("TTL : Pasuruan 19 mei 2004");
+    }
+    
+    public void infoAlamat() {
+        System.out.println("Alamat : Desa ngembal Kec.Tutur");
+    }
+    public void infoHobi() {
+        System.out.println("Hobi : Mencintai Semua Wanita");
+    }
+}
---
+++ utama.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mohammad__Fauzi
+ */
+public class utama {
+    public static void main(String [] args){
+        biodata biodata = new biodata ();
+        biodata.infoNama();
+        biodata.infoKelas();
+        biodata.infoAbsen();
+        biodata.infoTTL();
+        biodata.infoAlamat();
+        biodata.infoHobi();
+    }
+}
